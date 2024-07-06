5df385b69edfa86a804a05ce31d92d2ae187cef8
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2024-07-07 01:01:43 +0200

    Delete shit, organise

--- SCMD List Manager.py
+++ SCMD List Manager.py
@@ -1,166 +1,143 @@
-#This is where all the magic really happens.
-#If you're going to use some of my code be nice and make your app open source! (And by the way give me some credit *coughs*)
-#If you can let me know too, I'd be excited to hear someone uses something I made!
-
 import subprocess
 from bs4 import BeautifulSoup
 import requests
 import json
 import re
-try:
-    with open('./data/data.json','r') as f:
-        data=json.load(f)
-    data=data
-    try:
-        with open('./data/download.json','r') as f:
-            download=json.load(f)
-        download=download
-    except:
+from typing import List, Optional, Dict, Any
+
+class SteamWorkshopDownloader:
+    def __init__(self, data_path: str = './data/data.json', download_path: str = './data/download.json'):
+        self.data_path = data_path
+        self.download_path = download_path
+        self.data = self.load_json(self.data_path)
+        self.download = self.load_json(self.download_path)
+        self.appcondition = 'https://steamcommunity.com/app/'
+        self.itemcondition = 'https://steamcommunity.com/sharedfiles/filedetails/?id='
+        self.collectioncondition = 'https://steamcommunity.com/workshop/browse/?section=collections&appid='
+        self.scriptadd = ''
+
+    def load_json(self, path: str) -> Optional[Dict[str, Any]]:
+        try:
+            with open(path, 'r') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            print(f"ERROR: File at {path} not found.")
+            return None
+
+    def handle_error_3(self):
         print('SCMD List Manager & SCMD Workshop Downloader made by Berdy Alexei\nERROR 3: SCMD List Manager should only be opened when called by SCMD Workshop Downloader\n<<Open SCMD Workshop Downloader instead>>')
-        close=input('\n\n### Press Enter to close this window ###\n\n')
+        input('\n\n### Press Enter to close this window ###\n\n')
         exit()
-    appcondition='https://steamcommunity.com/app/'
-    itemcondition='https://steamcommunity.com/sharedfiles/filedetails/?id='
-    collectioncondition='https://steamcommunity.com/workshop/browse/?section=collections&appid='
-    scriptadd=''
-    repeat=data["repeat"]
-    if not download:
-        print('SCMD List Manager & SCMD Workshop Downloader made by Berdy Alexei\nERROR 3: SCMD List Manager should only be opened when called by SCMD Workshop Downloader\n<<Open SCMD Workshop Downloader instead>>')
-        close=input('\n\n### Press Enter to close this window ###\n\n')
-        exit()
-    else:
-        if data["mode"]==1 or data["mode"]==3 or data["mode"]==5:
-            gIDi=[]
-            items=[]
-            collections=[]
-            successcount=0
-            errorcount=0
-            print('Analysing links... (Multiple-mode)')
-            for coln in range(len(download['list'])):
-                print(f'Classifying links: {coln+1} of {len(download["list"])}')
-                collectiondetector=0
-                try:
-                    for link in BeautifulSoup(requests.get(download['list'][coln]).text, 'html.parser').find_all('a'):
-                        if collectioncondition in str(link.get('href')):
-                            collectiondetector+=1
-                    if collectiondetector<1:
-                        items.append(download['list'][coln])
-                        successcount+=1
-                    else:
-                        collections.append(download['list'][coln])
-                        successcount+=1
-                except:
-                    errorcount+=1
-            for coln in range(len(items)):
-                print(f'Analyzing items links: {coln+1} of {len(items)}')
-                try:
-                    for link in BeautifulSoup(requests.get(items[coln]).text, 'html.parser').find_all('a'):
-                        if appcondition in str(link.get('href')):
-                            duo=(str(link.get('href')))
-                    gIDi+=re.findall('\d+',str(duo))
-                    wIDi=re.findall('\d+',str(items))
-                except:
-                    errorcount+=1
-                    successcount-=1
+
+    def classify_links(self, links: List[str]) -> (List[str], List[str], int, int):
+        items, collections = [], []
+        successcount, errorcount = 0, 0
+        for coln, link in enumerate(links):
+            print(f'Classifying links: {coln + 1} of {len(links)}')
             try:
-                for wIDc in range(len(items)):
-                    scriptadd+=f' +workshop_download_item {gIDi[wIDc]} {wIDi[wIDc]} validate'
-                    if data["bscim"]==True:
-                        for i in range(repeat):
-                            scriptadd+=f' +workshop_download_item {gIDi[0]} {wIDi[wIDc]} validate'
-                        print(f'BSIM Treatment given to item #{wIDc+1} repeated {repeat} time/s.')
-            except:
-                pass
-            downloadlist=[]
-            for coln in range(len(collections)):
-                print(f'Analyzing collections links: {coln+1} of {len(collections)}')
-                for link in BeautifulSoup(requests.get(collections[coln]).text, 'html.parser').find_all('a'):
-                    if itemcondition in str(link.get('href')):
-                        downloadlist.append(str(link.get('href')))
-                downloadlist.remove(f'{collections[coln]}')
-                downloadlist=re.findall('\d+',str(downloadlist))
-                for dupe in downloadlist:
-                    if downloadlist.count(dupe)>1:
-                        downloadlist.remove(dupe)
-                for link in BeautifulSoup(requests.get(download['list'][0]).text, 'html.parser').find_all('a'):
-                    if appcondition in str(link.get('href')):
-                        applink=str(link.get('href'))
-                wIDi=downloadlist
-                gIDi=re.findall('\d+',str(applink))
-                for wIDc in range(len(wIDi)):
-                    scriptadd+=f' +workshop_download_item {gIDi[0]} {wIDi[wIDc]} validate'
-                    if data["bscim"]==True:
-                        for i in range(repeat):
-                            scriptadd+=f' +workshop_download_item {gIDi[0]} {wIDi[wIDc]} validate'
-                if data["bscim"]==True:       
-                    print(f'BSIM Treatment given to {len(wIDi)} items {repeat} time/s.')
-                print(f'Registered {len(wIDi)} items.')
-            if (errorcount>0 and successcount==0) or successcount==0:
-                print(f'ERROR 2: All introduced links are wrong')
-                print('<< Having a bad Internet conection can also make this error to happen. Try again later. >>')
-                with open('./data/download.json','w') as f:
-                    json.dump('',f)
+                soup = BeautifulSoup(requests.get(link).text, 'html.parser')
+                collectiondetector = sum(1 for a in soup.find_all('a') if self.collectioncondition in str(a.get('href')))
+                if collectiondetector < 1:
+                    items.append(link)
+                else:
+                    collections.append(link)
+                successcount += 1
+            except Exception as e:
+                print(f"Error classifying link {link}: {e}")
+                errorcount += 1
+        return items, collections, successcount, errorcount
+
+    def analyze_items(self, items: List[str]) -> (List[str], List[str], int):
+        gIDi, wIDi = [], []
+        errorcount = 0
+        for coln, item in enumerate(items):
+            print(f'Analyzing items links: {coln + 1} of {len(items)}')
+            try:
+                soup = BeautifulSoup(requests.get(item).text, 'html.parser')
+                for link in soup.find_all('a'):
+                    if self.appcondition in str(link.get('href')):
+                        duo = str(link.get('href'))
+                        break
+                gIDi.extend(re.findall('\d+', str(duo)))
+                wIDi.extend(re.findall('\d+', str(item)))
+            except Exception as e:
+                print(f"Error analyzing item {item}: {e}")
+                errorcount += 1
+        return gIDi, wIDi, errorcount
+
+    def generate_script(self, gIDi: List[str], wIDi: List[str], repeat: int, bscim: bool) -> str:
+        scriptadd = ''
+        for wIDc in range(len(wIDi)):
+            scriptadd += f' +workshop_download_item {gIDi[wIDc]} {wIDi[wIDc]} validate'
+            if bscim:
+                for _ in range(repeat):
+                    scriptadd += f' +workshop_download_item {gIDi[0]} {wIDi[wIDc]} validate'
+                print(f'BSIM Treatment given to item #{wIDc + 1} repeated {repeat} time/s.')
+        return scriptadd
+
+    def run(self):
+        if not self.download:
+            self.handle_error_3()
+
+        repeat = self.data["repeat"]
+        mode = self.data["mode"]
+        bscim = self.data["bscim"]
+
+        if mode in [1, 3, 5]:
+            items, collections, successcount, errorcount = self.classify_links(self.download['list'])
+            gIDi, wIDi, item_errors = self.analyze_items(items)
+            errorcount += item_errors
+            self.scriptadd = self.generate_script(gIDi, wIDi, repeat, bscim)
+
+            if (errorcount > 0 and successcount == 0) or successcount == 0:
+                print('ERROR 2: All introduced links are wrong')
+                print('<< Having a bad Internet connection can also make this error to happen. Try again later. >>')
             else:
-                script=download["script"]+scriptadd
-                datetime=download["datetime"]
-                with open('./data/download.json','w') as f:
-                    json.dump('',f)
-                if data["mode"]==3 or data["mode"]==5:
-                    writer=open(f'./generated scripts/script {datetime[0]}.bat','w')
-                    writer.write(script)
-                    writer.close()
-                    print(f'Script generated as: script {datetime[0]}.bat')
-                if  data["mode"]==1 or data["mode"]==5:
-                    try:
-                        print('Download started\n')
-                        subprocess.call('{}'.format(script))
-                    except FileNotFoundError:
-                        if data["mode"]==3 or data["mode"]==5:
-                            print("\nERROR 0: Too many elements tried to be downloaded. Close this Window and try again introducing less.\nATTENTION: The created script probably won't work either because of that.\n")
-                        else:
-                            print('\nERROR 0: Too many elements tried to be downloaded. Close this Window and try again introducing less.\n')
+                script = self.download["script"] + self.scriptadd
+                datetime = self.download["datetime"]
+                self.save_json('', self.download_path)
+                if mode in [3, 5]:
+                    self.write_script(script, datetime[0])
+                if mode in [1, 5]:
+                    self.execute_script(script)
         else:
             print('Analysing links... (Single-mode)')
             try:
-                for link in BeautifulSoup(requests.get(download["list"][0]).text, 'html.parser').find_all('a'):
-                    if appcondition in str(link.get('href')):
-                        applink=str(link.get('href'))
-                gIDi=re.findall('\d+',str(applink))
-                wIDi=re.findall('\d+',str(download["list"]))
-                for wIDc in range(len(wIDi)):
-                    print(f'Analyzing items links: {wIDc+1} of {len(wIDi)}')
-                    scriptadd+=f' +workshop_download_item {gIDi[0]} {wIDi[wIDc]} validate'
-                    if data["bscim"]==True:
-                        for i in range(repeat):
-                            scriptadd+=f' +workshop_download_item {gIDi[0]} {wIDi[wIDc]} validate'
-                        print(f'BSIM Treatment given to item #{wIDc+1} repeated {repeat} time/s')
-                script=download["script"]+scriptadd
-                if data["mode"]==2 or data["mode"]==4:
-                    datetime=download["datetime"]
-                    writer=open(f'./generated scripts/script {datetime[0]}.bat','w')
-                    writer.write(script)
-                    writer.close()
-                    print(f'Script generated as: script {datetime[0]}.bat')
-                if data["mode"]==0 or data["mode"]==4:
-                    try:
-                        with open('./data/download.json','w') as f:
-                            json.dump('',f)
-                        print('Download started\n')
-                        subprocess.call('{}'.format(script))
-                    except FileNotFoundError:
-                        if data["mode"]==2 or data["mode"]==4:
-                            print("\nERROR 0: Too many elements tried to be downloaded. Close this Window and try again introducing less.\nATTENTION: The created script probably won't work either because of that.\n")
-                        else:
-                            print('\nERROR 0: Too many elements tried to be downloaded. Close this Window and try again introducing less.\n')
-            except:
-                print('ERROR 1: The first link entered is incorrect')
-                print('<< Having a bad Internet conection can also make this error to happen. Try again later. >>')
-        with open('./data/download.json','w') as f:
-            json.dump('',f)
-except:
-    print('ERROR 4: Required files could not be accessed')
-    print('<< In SCMD Workshop Downloader press the configuration button and access the ERRORS & SOLUTIONS tab to know the details and how to solve the problem. >>')
-try:
-    close=input('\n\n### Press Enter to close this window ###\n\n')
-except:
-    pass
+                soup = BeautifulSoup(requests.get(self.download["list"][0]).text, 'html.parser')
+                for link in soup.find_all('a'):
+                    if self.appcondition in str(link.get('href')):
+                        applink = str(link.get('href'))
+                        break
+                gIDi = re.findall('\d+', str(applink))
+                wIDi = re.findall('\d+', str(self.download["list"]))
+                self.scriptadd = self.generate_script(gIDi, wIDi, repeat, bscim)
+                script = self.download["script"] + self.scriptadd
+                if mode in [2, 4]:
+                    self.write_script(script, self.download["datetime"][0])
+                if mode in [0, 4]:
+                    self.execute_script(script)
+            except Exception as e:
+                print(f"ERROR 1: The first link entered is incorrect: {e}")
+                print('<< Having a bad Internet connection can also make this error to happen. Try again later. >>')
+
+        self.save_json('', self.download_path)
+
+    def write_script(self, script: str, datetime: str):
+        with open(f'./generated scripts/script {datetime}.bat', 'w') as writer:
+            writer.write(script)
+        print(f'Script generated as: script {datetime}.bat')
+
+    def execute_script(self, script: str):
+        try:
+            print('Download started\n')
+            subprocess.call(script)
+        except FileNotFoundError:
+            print('\nERROR 0: Too many elements tried to be downloaded. Close this Window and try again introducing less.\n')
+
+    def save_json(self, data: Any, path: str):
+        with open(path, 'w') as f:
+            json.dump(data, f)
+
+if __name__ == "__main__":
+    downloader = SteamWorkshopDownloader()
+    downloader.run()
