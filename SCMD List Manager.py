88563b35f3129a52e3c13cd3bbb03e44fb45ce43
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2024-07-07 01:10:40 +0200

    Clean, organise, note functionality

--- SCMD List Manager.py
+++ SCMD List Manager.py
@@ -6,17 +6,32 @@ import re
 from typing import List, Optional, Dict, Any
 
 class SteamWorkshopDownloader:
+    
+    # Manage and download items from the Steam Workshop.
+    
     def __init__(self, data_path: str = './data/data.json', download_path: str = './data/download.json'):
+        
+        # Initialise the Downloader with paths to configuration files.
+        
+        # :param data_path: Path to the data configuration file.
+        # :param download_path: Path to the download configuration file.
+        
         self.data_path = data_path
         self.download_path = download_path
-        self.data = self.load_json(self.data_path)
-        self.download = self.load_json(self.download_path)
+        self.data = self.load_json(self.data_path)  # Load data configuration
+        self.download = self.load_json(self.download_path)  # Load download configuration
         self.appcondition = 'https://steamcommunity.com/app/'
         self.itemcondition = 'https://steamcommunity.com/sharedfiles/filedetails/?id='
         self.collectioncondition = 'https://steamcommunity.com/workshop/browse/?section=collections&appid='
-        self.scriptadd = ''
+        self.scriptadd = ''  # Initialize the script addition string
 
     def load_json(self, path: str) -> Optional[Dict[str, Any]]:
+        
+        # Load JSON data from a file.
+        
+        # :param path: Path to the JSON file.
+        # :return: JSON data as a dictionary or None if file not found.
+        
         try:
             with open(path, 'r') as f:
                 return json.load(f)
@@ -25,11 +40,20 @@ class SteamWorkshopDownloader:
             return None
 
     def handle_error_3(self):
+        
+        # Handle error 3 by printing a message and exiting the program.
+        
         print('SCMD List Manager & SCMD Workshop Downloader made by Berdy Alexei\nERROR 3: SCMD List Manager should only be opened when called by SCMD Workshop Downloader\n<<Open SCMD Workshop Downloader instead>>')
         input('\n\n### Press Enter to close this window ###\n\n')
         exit()
 
     def classify_links(self, links: List[str]) -> (List[str], List[str], int, int):
+        
+        # Classify links into items and collections.
+        
+        # :param links: List of links to classify.
+        # :return: Lists of items and collections, and counts of successful and error classifications.
+        
         items, collections = [], []
         successcount, errorcount = 0, 0
         for coln, link in enumerate(links):
@@ -48,6 +72,12 @@ class SteamWorkshopDownloader:
         return items, collections, successcount, errorcount
 
     def analyze_items(self, items: List[str]) -> (List[str], List[str], int):
+        
+        # Analyze items to extract game IDs and workshop item IDs.
+        
+        # :param items: List of item links.
+        # :return: Lists of game IDs and workshop item IDs, and a count of errors.
+        
         gIDi, wIDi = [], []
         errorcount = 0
         for coln, item in enumerate(items):
@@ -66,6 +96,15 @@ class SteamWorkshopDownloader:
         return gIDi, wIDi, errorcount
 
     def generate_script(self, gIDi: List[str], wIDi: List[str], repeat: int, bscim: bool) -> str:
+        
+        # Generate the script for downloading workshop items.
+        
+        # :param gIDi: List of game IDs.
+        # :param wIDi: List of workshop item IDs.
+        # :param repeat: Number of times to repeat the download.
+        # :param bscim: Boolean to determine if BSIM treatment is needed.
+        # :return: The generated script as a string.
+        
         scriptadd = ''
         for wIDc in range(len(wIDi)):
             scriptadd += f' +workshop_download_item {gIDi[wIDc]} {wIDi[wIDc]} validate'
@@ -76,6 +115,9 @@ class SteamWorkshopDownloader:
         return scriptadd
 
     def run(self):
+        
+        # Main execution method to run the downloader.
+        
         if not self.download:
             self.handle_error_3()
 
@@ -123,11 +165,22 @@ class SteamWorkshopDownloader:
         self.save_json('', self.download_path)
 
     def write_script(self, script: str, datetime: str):
+        
+        # Write the generated script to a file.
+        
+        # :param script: The script to write.
+        # :param datetime: The datetime string to use in the filename.
+        
         with open(f'./generated scripts/script {datetime}.bat', 'w') as writer:
             writer.write(script)
         print(f'Script generated as: script {datetime}.bat')
 
     def execute_script(self, script: str):
+        
+        # Execute the generated script.
+        #
+        # :param script: The script to execute.
+        
         try:
             print('Download started\n')
             subprocess.call(script)
@@ -135,6 +188,12 @@ class SteamWorkshopDownloader:
             print('\nERROR 0: Too many elements tried to be downloaded. Close this Window and try again introducing less.\n')
 
     def save_json(self, data: Any, path: str):
+        #
+        # Save data to a JSON file.
+        #
+        # :param data: The data to save.
+        # :param path: The path to the JSON file.
+        #
         with open(path, 'w') as f:
             json.dump(data, f)
 
